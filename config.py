137696a233219a125a9d65b0703c5a5b7df816a1
Jason Satti <48844684+JasonSatti@users.noreply.github.com>
2019-04-12 19:04:37 -0400

    update resource_url veriable

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 api_user = 'API_Username'
 api_pw = 'API_Password'
-resource_url = 'JAMF_Resource_URL'
+resource_url = 'https://company.jamfcloud.com/JSSResource/'
 script_api_url = 'scripts/id/'
 url = resource_url + script_api_url
 script_dir = 'Local_Folder_Name'
